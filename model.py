094e70737744c5f5c6061833abfbd45002f2ab4d
Genius1237 <anirudhsriniv@gmail.com>
2019-04-30 23:29:42 -0400

    Move file read functions to separate files

--- model.py
+++ model.py
@@ -2,14 +2,7 @@ import torchvision
 import torch
 import torch.nn as nn
 
-model = torchvision.models.alexnet(pretrained=False)
+def get_model(pretrained=False):
+    model = torchvision.models.alexnet(pretrained)
 
-t = torch.randn(1,3,360,640)
-print(t.shape)
-t = model.features(t)
-print(t.shape)
-t = model.avgpool(t)
-print(t.shape)
-t = t.view(-1,256*6*6)
-t = model.classifier(t)
-print(t.shape)
+    return model
--- screen_capture.py
+++ screen_capture.py
@@ -4,34 +4,7 @@ from directkeys import PressKey, ReleaseKey, W,S,A,D
 import random
 import time
 import cv2
-import pickle
-from multiprocessing import Process, Pipe
-
-class ParallelWriter():
-    
-    @staticmethod
-    def worker(conn):
-        while(True):
-            data = conn.recv()
-            pickle.dump(data,open('{}.pickle'.format(time.time()),'wb'))
-    
-    def __init__(self,limit):
-        self.local,self.remote = Pipe()
-        self.process = Process(target=ParallelWriter.worker, args=(self.remote,))
-        self.process.start()
-        self.remote.close()
-        self.list = []
-        self.len = 0
-        self.limit = limit
-    
-    def append(self,item):
-        self.list.append(item)
-        self.len+=1
-
-        if self.len == self.limit:
-            self.local.send(self.list)
-            self.list = []
-            self.len = 0
+import utils
 
 if __name__ == "__main__":  
     '''
@@ -41,18 +14,20 @@ if __name__ == "__main__":
         time.sleep(1)
         ReleaseKey(W)
     '''
-    p = ParallelWriter(limit=100)
+    p = utils.ParallelWriter('temp',limit=64)
     t = time.time()
 
-    while(True):
-        #image = grab_screen(window_title='Grand Theft AUto V',region=(1,31,1067,630))
-        image = grab_screen(region=(1,31,1067,630))
-        keys = key_check()
-        image = cv2.resize(image,(640,360))
-        p.append((image,keys))
-        #pickle.dump((image,keys),open('{}.pickle'.format(t),'wb'))
-        #data.append((image,keys))
-        t_new = time.time()
-        print((t_new-t)*1000)
-        t = t_new
-    
+    try:
+        while(True):
+            #image = grab_screen(window_title='Grand Theft AUto V',region=(1,31,1067,630))
+            image = grab_screen(region=(1,31,1067,630))
+            keys = key_check()
+            image = cv2.resize(image,(640,360))
+            p.append((image,keys))
+            #pickle.dump((image,keys),open('{}.pickle'.format(t),'wb'))
+            #data.append((image,keys))
+            t_new = time.time()
+            print((t_new-t)*1000)
+            t = t_new
+    except KeyboardInterrupt:
+        del p    
